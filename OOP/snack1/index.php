7386f7a73b50e908131f4d8ec6e6c0e4c110642c
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-25 12:41:18 +0100

    create 5 proprietà

---
+++ OOP/snack1/index.php
@@ -0,0 +1,20 @@
+<!-- Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, tre metodi ed il costruttore.
+Microbonus: creo un'altra classe che aggiungo al pokemon per composizione che configuri il tipo di pokemon (aria, terra, ecc.) -->
+
+<?php 
+
+class charizard {
+public $tipo;
+public $categoria;
+public $abilita;
+public $descrizione;
+public $megaevoluzione;
+}
+
+$tipo = 'Fuoco/Volante';
+$categoria = 'Fiamma';
+$abilità = 'Aiutofuoco';
+$descrizione = 'Si dice che la fiamma di Charizard abbia una temperatura tanto più alta quanto più dure sono le lotte che ha affrontato.';
+$megaevoluzione = true
+
+?>
